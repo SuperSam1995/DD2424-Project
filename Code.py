1f207bd9c503d94552bf517bf1d1bb1d085def04
”Oscar <Oscar@azrak.se>
2023-05-18 16:40:56 +0200

    3 av 4 punkter klart Edelen

--- Code.py
+++ Code.py
@@ -28,6 +28,7 @@ def prep_pixels(train, test):
 	# return normalized images
 	return train_norm, test_norm
 
+
 # define cnn model
 def define_model():
 	model = Sequential()
@@ -62,7 +63,7 @@ def run_test_harness():
 	model.save('final_model.h5')
 
 # entry point, run the test harness
-run_test_harness()
+#run_test_harness()
 
 # evaluate the deep model on the test dataset
 from keras.datasets import cifar10
@@ -78,23 +79,18 @@ def load_dataset():
 	testY = to_categorical(testY)
 	return trainX, trainY, testX, testY
 
-# scale pixels
-def prep_pixels(train, test):
-	# convert from integers to floats
-	train_norm = train.astype('float32')
-	test_norm = test.astype('float32')
-	# normalize to range 0-1
-	train_norm = train_norm / 255.0
-	test_norm = test_norm / 255.0
-	# return normalized images
-	return train_norm, test_norm
+
+
+
+
+
 
 # run the test harness for evaluating a model
 def run_test_harness_load():
 	# load dataset
 	trainX, trainY, testX, testY = load_dataset()
 	# prepare pixel data
-	trainX, testX = prep_pixels(trainX, testX)
+	trainX, testX = prep_pixels_2(trainX, testX)
 	# load model
 	model = load_model('final_model.h5')
 	# evaluate model on test dataset
@@ -102,4 +98,186 @@ def run_test_harness_load():
 	print('> %.3f' % (acc * 100.0))
 
 # entry point, run the test harness
+print("Final model performance")
 run_test_harness_load()
+
+# See if normalizing the data to have zero mean and standard deviation 1 improves performance
+
+def prep_pixels_2(train, test):
+	# convert from integers to floats
+	train_norm = train.astype('float32')
+	test_norm = test.astype('float32')
+	# calculate mean and standard deviation
+	train_mean, train_std = train_norm.mean(), train_norm.std()
+	test_mean, test_std = test_norm.mean(), test_norm.std()
+	# global standardization of pixels
+	train_norm = (train_norm - train_mean) / train_std
+	test_norm = (test_norm - test_mean) / test_std
+	# return normalized images
+	return train_norm, test_norm
+
+# run the test harness for evaluating a model
+def run_test_harness_1task():
+	# load dataset
+    trainX, trainY, testX, testY = load_dataset()
+    # prepare pixel data
+    trainX, testX = prep_pixels_2(trainX, testX)
+    # define model
+    model = define_model()
+    # fit model
+    model.fit(trainX, trainY, epochs=100, batch_size=64, verbose=0)
+    # save model
+    model.save('final_model_1task.h5')
+
+# entry point, run the test harness
+run_test_harness_1task()
+
+# evaluate the deep model on the test dataset
+from keras.datasets import cifar10
+from keras.models import load_model
+from keras.utils import to_categorical
+
+def run_test_harness_load_1task():
+	# load dataset
+	trainX, trainY, testX, testY = load_dataset()
+	# prepare pixel data
+	trainX, testX = prep_pixels_2(trainX, testX)
+	# load model
+	model = load_model('final_model_1task.h5')
+	# evaluate model on test dataset
+	_, acc = model.evaluate(testX, testY, verbose=0)
+	print('> %.3f' % (acc * 100.0))
+	
+# entry point, run the test harness
+print("Task 1 performance: ")
+run_test_harness_load_1task()
+
+
+## task 2, Replace the SGD + momentum optimizer with Adam and then AdamW. Do these optimizers lead to better performance and/or faster convergence?
+import keras.optimizers
+
+
+def define_model_2task(optimizer='SGD'):
+	model = Sequential()
+	model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same', input_shape=(32, 32, 3)))
+	model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+	model.add(MaxPooling2D((2, 2)))
+	model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+	model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+	model.add(MaxPooling2D((2, 2)))
+	model.add(Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+	model.add(Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+	model.add(MaxPooling2D((2, 2)))
+	model.add(Flatten())
+	model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
+	model.add(Dense(10, activation='softmax'))
+	# compile model
+	if optimizer == 'SGD':
+		opt = SGD(lr=0.001, momentum=0.9)
+	elif optimizer == 'Adam':
+		opt = keras.optimizers.Adam()
+	model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
+	return model
+
+# run the test harness for evaluating a model
+
+def run_test_harness_2task_adam():
+	# load dataset
+    trainX, trainY, testX, testY = load_dataset()
+    # prepare pixel data
+    trainX, testX = prep_pixels_2(trainX, testX)
+    # define model
+    model = define_model_2task('Adam')
+    # fit model
+    model.fit(trainX, trainY, epochs=100, batch_size=64, verbose=0)
+    # save model
+    model.save('final_model_2task_adam.h5')
+
+# entry point, run the test harness
+run_test_harness_2task_adam()
+
+# evaluate the deep model on the test dataset
+
+
+def run_test_harness_load_2task_adam():
+	# load dataset
+    trainX, trainY, testX, testY = load_dataset()
+    # prepare pixel data
+    trainX, testX = prep_pixels_2(trainX, testX)
+    # load model
+    model = load_model('final_model_2task_adam.h5')
+    # evaluate model on test dataset
+    _, acc = model.evaluate(testX, testY, verbose=0)
+    print('> %.3f' % (acc * 100.0))
+
+# entry point, run the test harness
+print("Task 2 Adam performance: ")
+
+run_test_harness_load_2task_adam()
+
+# AdamW
+# import tsensorfow addons
+import tensorflow_addons as tfa
+
+
+
+def define_model_2task(optimizer='SGD'):
+	model = Sequential()
+	model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same', input_shape=(32, 32, 3)))
+	model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+	model.add(MaxPooling2D((2, 2)))
+	model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+	model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+	model.add(MaxPooling2D((2, 2)))
+	model.add(Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+	model.add(Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+	model.add(MaxPooling2D((2, 2)))
+	model.add(Flatten())
+	model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
+	model.add(Dense(10, activation='softmax'))
+	# compile model
+	if optimizer == 'SGD':
+		opt = SGD(lr=0.001, momentum=0.9)
+	elif optimizer == 'Adam':
+		opt = keras.optimizers.Adam()
+	elif optimizer == 'AdamW':
+        opt = tfa.optimizers.AdamW(learning_rate=0.001, weight_decay=0.0001)		
+	model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
+	return model
+	
+# run the test harness for evaluating a model
+
+def run_test_harness_2task_adam2():
+	# load dataset
+    trainX, trainY, testX, testY = load_dataset()
+    # prepare pixel data
+    trainX, testX = prep_pixels_2(trainX, testX)
+    # define model
+    model = define_model_2task('AdamW')
+    # fit model
+    model.fit(trainX, trainY, epochs=100, batch_size=64, verbose=0)
+    # save model
+    model.save('final_model_2task_adamW.h5')
+
+# entry point, run the test harness
+run_test_harness_2task_adam2()
+
+# evaluate the deep model on the test dataset
+
+def run_test_harness_load_2task_adamW():
+    # load dataset
+    trainX, trainY, testX, testY = load_dataset()
+    # prepare pixel data
+    trainX, testX = prep_pixels_2(trainX, testX)
+    # load model
+    model = load_model('final_model_2task_adamW.h5')
+    # evaluate model on test dataset
+    _, acc = model.evaluate(testX, testY, verbose=0)
+    print('> %.3f' % (acc * 100.0))
+
+# entry point, run the test harness
+print("Task 2 AdamW performance: ")
+run_test_harness_load_2task_adamW()
+
+
+#task 3: Try different learning rate schedulers such as learning rate warm-up + cosine annealing, step decay or cosine annealing with re-starts and see how is helps/affects training.
